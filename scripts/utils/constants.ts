24d8c7e6d270d32b80550c9f94e0592dcfdb938e
YeomanLi <Yeoman_Li@Outlook.com>
2020-04-14 18:24:33 +0800

    🎉 init finished

---
+++ scripts/utils/constants.ts
@@ -0,0 +1,24 @@
+import path from 'path';
+import { argv } from 'yargs';
+
+const __DEV__ = process.env.NODE_ENV !== 'production';
+const ENABLE_ANALYZE = !!argv.analyze;
+const ENABLE_OPEN = !!argv.open;
+
+const HOST = '127.0.0.1';
+const DEFAULT_PORT = 3000;
+
+const PROJECT_ROOT = path.resolve(__dirname, '../../');
+const PROJECT_NAME = path.parse(PROJECT_ROOT).name;
+const HMR_PATH = '/__webpack_hmr';
+
+export {
+  __DEV__,
+  ENABLE_ANALYZE,
+  ENABLE_OPEN,
+  HOST,
+  DEFAULT_PORT,
+  PROJECT_ROOT,
+  PROJECT_NAME,
+  HMR_PATH
+};
